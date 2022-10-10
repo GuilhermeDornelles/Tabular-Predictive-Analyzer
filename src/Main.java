6f73ade343cefee8d348e9be9b0f6c46fb793063
guilherme.dornelles <guilherme.dornelles@nubo.ai>
2022-10-10 16:59:31 -0300

    Adding a args logic to run the class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        // TabularAnalyzer analyzer = new TabularAnalyzer(args[0]);
-        TabularAnalyzer analyzer = new TabularAnalyzer("./input.txt");
+        TabularAnalyzer analyzer = new TabularAnalyzer(args[0]);
+        // TabularAnalyzer analyzer = new TabularAnalyzer("../input.txt");
         System.out.println(analyzer.printAllFirst());
     }
 }
